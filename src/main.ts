739671dc9d7d6e230fe5b9df69038c10033df2f8
rkhanna36 <rkhanna36@dxc.com>
2020-03-18 17:20:15 +0530

    first

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports'; 
+import 'zone.js';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+Amplify.configure(awsconfig);
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +14,4 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+  
